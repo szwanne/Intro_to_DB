8b488e687d7aa20fbc13c98a86c833d58ff57c20
szwanne <103660814+szwanne@users.noreply.github.com>
2025-06-29 19:18:12 +0200

    Add four customers data in the customers TABLE

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
